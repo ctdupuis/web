7632a957d5fbe111892b301cd683d0894697cda9
ctdupuis <ctdupuis7@gmail.com>
2021-10-29 15:42:12 -0500

    part 4 in the books

---
+++ profile.js
@@ -0,0 +1,9 @@
+document.getElementById('color').addEventListener('click', (e) => {
+    alert("My favorite color is red")
+})
+document.getElementById('place').addEventListener('click', (e) => {
+    alert("There's no place like home")
+})
+document.getElementById('ritual').addEventListener('click', (e) => {
+    alert("I love mornings in front of my computer with a hot cup of coffee")
+})
