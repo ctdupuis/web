abe573863806e94ae3d5ee5d326d519f0a7db92b
ctdupuis <ctdupuis7@gmail.com>
2021-10-29 15:30:12 -0500

    part 2 done

--- about.js
+++ about.js
@@ -5,10 +5,16 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert(`Your message is ${document.getElementById('message').value}`)
 }
 
 
 let form = document.querySelector('form#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+let duck = document.getElementById('duck');
+
+duck.addEventListener('mouseover', (e) => {
+	alert("Well don't you just look FABULOUS")
+})
